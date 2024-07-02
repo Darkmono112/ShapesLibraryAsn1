5cf3ab756e59745ddd9318675a1e69491f94e87b
Robert Gordon <darkmono37@gmail.com>
2024-07-01 18:06:08 -0600

    finished triangle

--- Assignment1/src/main/kotlin/shapes/Triangle.kt
+++ Assignment1/src/main/kotlin/shapes/Triangle.kt
@@ -1,9 +1,35 @@
 package org.example.shapes
 
-class Triangle(var verticies:List<Point>) {
+import kotlin.math.abs
 
-    fun testVer(){
-        println(verticies)
+class Triangle(pointA: Point, pointB:Point, pointC:Point): Moveable, Area {
+
+    init {
+        //Make sure the slop is not the same between these sets, otherwise they are on a line
+        require((pointA.y-pointB.y)/(pointA.x - pointB.x) != (pointA.y-pointC.y)/(pointA.x - pointC.x))
     }
+
+    var pointA = pointA
+        private set
+    var pointB = pointB
+        private set
+    var pointC = pointC
+        private set
+
+    override fun move(deltaX: Double, deltaY: Double) {
+        pointA.move(deltaX, deltaY)
+        pointC.move(deltaX, deltaY)
+        pointB.move(deltaX, deltaY)
+    }
+
+    override fun area(): Double {
+        //Using the determinate to find the area
+        return abs(.5*((pointB.x * pointC.y - pointB.y*pointC.x) -
+                (pointA.x * pointC.y - pointA.y * pointC.x) +
+                (pointA.x*pointB.y - pointA.y * pointB.x)))
+
+
+    }
+
 
 }
---
+++ Assignment1/src/test/kotlin/shapes/TriangleTest.kt
@@ -0,0 +1,42 @@
+package shapes
+
+import org.example.shapes.Point
+import org.example.shapes.Triangle
+import kotlin.concurrent.timer
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class TriangleTest {
+
+    //triangle with a know area of 2
+    private var triangle = Triangle(Point(0.0,0.0),Point(1.0,2.0), Point(2.0,0.0) )
+
+    @Test
+    fun move() {
+        triangle.move(0.0,10.0)
+        assertEquals(0.0 , triangle.pointA.x)
+        assertEquals(10.0 , triangle.pointA.y)
+        assertEquals(1.0 , triangle.pointB.x)
+        assertEquals(12.0 , triangle.pointB.y)
+        assertEquals(2.0 , triangle.pointC.x)
+        assertEquals(10.0 , triangle.pointC.y)
+
+        triangle.move(45.0,345.0)
+
+        assertEquals(0.0+45 , triangle.pointA.x)
+        assertEquals(10.0+345.0 , triangle.pointA.y)
+        assertEquals(1.0+45 , triangle.pointB.x)
+        assertEquals(12.0+345.0 , triangle.pointB.y)
+        assertEquals(2.0+45 , triangle.pointC.x)
+        assertEquals(10.0+345.0 , triangle.pointC.y)
+    }
+
+    @Test
+    fun area() {
+        assertEquals(2.0, triangle.area())
+        triangle.move(24.0,24.0)
+        assertEquals(2.0, triangle.area())
+
+
+    }
+}
